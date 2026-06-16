7d5d5ef6eaac8c643a0210f1d61f812f1a23bf11
Faik-man <faik199417@gmail.com>
2026-06-16 22:07:32 +0300

    Заменены объекты с динамическими свойствами на массивы в json форматировании

--- src/Formatters/JsonFormatter.php
+++ src/Formatters/JsonFormatter.php
@@ -8,12 +8,11 @@ class JsonFormatter implements FormatterInterface
 {
     public static function format(array $tree): string
     {
-        $json = self::createJsonObject($tree);
+        $json = self::createJson($tree);
 
-        $properties = get_object_vars($json);
         $result = array_map(
-            fn(string $propertyName): string => self::iterateProperty($propertyName, $properties[$propertyName]),
-            array_keys($properties)
+            fn(string $propertyName): string => self::iterateProperty($propertyName, $json[$propertyName]),
+            array_keys($json)
         );
 
         return "{\n" . implode(",\n", $result) . "\n}";
@@ -22,12 +21,12 @@ class JsonFormatter implements FormatterInterface
     private static function iterateProperty(string $propertyName, mixed $propertyValue, int $depth = 1): string
     {
         $spaces = str_repeat(' ', $depth * self::SPACES_COUNT);
-        if (!is_object($propertyValue)) {
+        if (!in_array(gettype($propertyValue), ['array', 'object'])) {
             $result = $spaces . self::toString($propertyName) . ': ' . self::toString($propertyValue);
             return $result;
         }
 
-        $innerProperties = get_object_vars($propertyValue);
+        $innerProperties = is_object($propertyValue) ? get_object_vars($propertyValue) : $propertyValue;
         $updatedProperties = array_map(
             fn(string $innerPropertyName): string => (
                 self::iterateProperty(
@@ -47,18 +46,18 @@ class JsonFormatter implements FormatterInterface
         return $result;
     }
 
-    private static function createJsonObject(array $tree): object
+    private static function createJson(array $tree): array
     {
         $json = array_reduce(
             $tree,
-            fn(object $acc, Node $node) => self::iterateTree($node, $acc),
-            new \stdClass()
+            fn(array $acc, Node $node): array => self::iterateTree($node, $acc),
+            []
         );
 
         return $json;
     }
 
-    private static function iterateTree(Node $node, object $acc): object
+    private static function iterateTree(Node $node, array $acc): array
     {
         $property = $node->getPropertyName();
         if ($node->isLeaf()) {
@@ -66,35 +65,29 @@ class JsonFormatter implements FormatterInterface
             $diffType = $node->getDiffType();
             switch ($diffType) {
                 case Node::REMOVED:
-                    /** @phpstan-ignore-next-line */
-                    $acc->removed ??= new \stdClass();
-                    $acc->removed->$property = $value;
+                    $acc['removed'][$property] = $value;
                     break;
                 case Node::ADDED:
-                    /** @phpstan-ignore-next-line */
-                    $acc->added ??= new \stdClass();
-                    $acc->added->$property = $value;
+                    $acc['added'][$property] = $value;
                     break;
                 case Node::UPDATED:
-                    /** @phpstan-ignore-next-line */
-                    $acc->updated ??= new \stdClass();
-                    $propertyValue = new \stdClass();
-                    $propertyValue->old = $value['oldValue'];
-                    $propertyValue->new = $value['newValue'];
-                    $acc->updated->$property = $propertyValue;
+                    $acc['updated'][$property] = [
+                        'old' => $value['oldValue'],
+                        'new' => $value['newValue'],
+                    ];
                     break;
                 default:
-                    $acc->$property = $value;
+                    $acc[$property] = $value;
                     break;
             }
 
             return $acc;
         }
 
-        $acc->$property = array_reduce(
+        $acc[$property] = array_reduce(
             $node->getChildren(),
-            fn(object $acc, Node $child) => self::iterateTree($child, $acc),
-            new \stdClass()
+            fn(array $acc, Node $child): array => self::iterateTree($child, $acc),
+            []
         );
 
         return $acc;
